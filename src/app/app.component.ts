f07548693612286b21034ae98a5e6394cbddc476
vijayganesh-sde <vijayganeshsreeram2003@gmail.com>
2022-02-17 00:38:00 +0530

    new commit
    Signed-off-by: vijayganesh-sde <vijayganeshsreeram2003@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import books from './json_files/entertainment_books.json';
+import teen_books from './json_files/teen_books.json';
+import fantasy_books from './json_files/fantasybooks.json';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,9 @@ import books from './json_files/entertainment_books.json';
 export class AppComponent {
   title = 'using-api';
   bookslist:{image:string,name:string,author:string,book_stars:string,img_paths:string}[]=books;
+  teen_bookslist:{image:string,name:string,author:string,book_stars:string}[]=teen_books;
+  fantasybookslist:{image:string,name:string,author:string,book_stars:string}[]=fantasy_books;
   item:any;
+  item1:any;
+  item2:any;
 }

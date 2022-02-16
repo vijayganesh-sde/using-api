7431aa6329468355d1011fe6a36841c97fe4b9fb
vijayganesh-sde <vijayganeshsreeram2003@gmail.com>
2022-02-17 01:39:34 +0530

    new comit
    Signed-off-by: vijayganesh-sde <vijayganeshsreeram2003@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import books from './json_files/entertainment_books.json';
 import teen_books from './json_files/teen_books.json';
 import fantasy_books from './json_files/fantasybooks.json';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,4 +17,21 @@ export class AppComponent {
   item:any;
   item1:any;
   item2:any;
+
+  @Input() search!:string;
+  constructor(private router:Router){ }
+  oncancel(){
+    this.router.navigateByUrl('/');
+  }
+  onsearch(){
+    for(var i=0; i<this.bookslist.length; i++){
+      if(this.bookslist[i].name==this.search){
+        this.router.navigateByUrl('/search');
+      }
+    }
+  }
+
+  
 }
+
+
